17dc2567637c903b4265f5504c09283767b739e0
Alex <alexlin.sfdx@gmail.com>
2023-09-23 15:17:55 +0800

    初始設定餐廳靜態頁面與路由設定

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const exhbs = expressHandlebars.create({
   defaultLayout: "main",
 });
 
+//設定express靜態資源midleware路徑
+app.use(express.static("public"));
+
 // 設定handlebars為template engine
 // 當你調用res.render('someView')，Express會查找名為someView.handlebars的文件（通常在一個名為views的目錄下），並使用handlebars模板引擎來渲染它。
 app.engine("handlebars", exhbs.engine);
@@ -18,7 +21,11 @@ app.set("view engine", "handlebars");
 
 //路由設定
 app.get("/", (req, res) => {
-  res.send("測試根目錄回應");
+  res.render("index");
+});
+
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  res.render("show");
 });
 
 //啟動伺服器
